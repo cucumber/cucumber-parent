346874072114d5cb630078192dc45af97c8bc3cc
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-10-12 00:38:50 +0100

    Fix signing key encryption

--- scripts/decrypt_signing_key.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-openssl aes-256-cbc -K $encrypted_dbe4de280e0d_key -iv $encrypted_dbe4de280e0d_iv -in scripts/codesigning.asc.enc -out scripts/codesigning.asc -d
--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -7,11 +7,9 @@ if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
   exit 0
 fi
 
-if [[ "$TRAVIS_BRANCH" != 'master' && -z "$TRAVIS_TAG" ]]; then
+if [[ "$TRAVIS_BRANCH" != "master" && -z "$TRAVIS_TAG" ]]; then
   echo "Skipping deploy (not master branch or tagged build)"
   exit 0
 fi
-source "${DIR}/decrypt_signing_key.sh"
 gpg -q --fast-import scripts/codesigning.asc
 source "${DIR}/mvn_deploy.sh"
-#source "${DIR}/mvn_upload_api_docs.sh"
