3b9c7a06947912a4aeecfd1e66b6c7197805ec7b
M.P. Korstanje <logarithmicwhale@gmail.com>
2019-08-01 20:20:27 +0200

    Add profile to sign sources on travis

--- scripts/mvn_deploy.sh
+++ scripts/mvn_deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
-mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
+mvn deploy -Psign-source-javadoc-travis --settings scripts/ci-settings.xml -DskipTests=true
